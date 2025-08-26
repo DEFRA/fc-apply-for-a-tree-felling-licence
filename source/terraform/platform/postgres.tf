f5b56e71bdde1cc23ae4d00e36217982a17afe68
Ruaidhri Brown <rbrown@harriscomputer.com>
2025-08-26 14:24:48 +0100

    Reintroduce platform directory with clean history

---
+++ source/terraform/platform/postgres.tf
@@ -0,0 +1,99 @@
+resource "azurerm_private_dns_zone" "database" {
+  name                = "flo-databases.postgres.database.azure.com"
+  resource_group_name = azurerm_resource_group.fs_flov2.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "database" {
+  name                  = "database"
+  private_dns_zone_name = azurerm_private_dns_zone.database.name
+  virtual_network_id    = azurerm_virtual_network.fs_flov2.id
+  resource_group_name   = azurerm_resource_group.fs_flov2.name
+}
+
+data "azurerm_virtual_network" "ms_production" {
+  name                = "ms_production"
+  resource_group_name = "ms_production"
+  provider            = azurerm.managedservices
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "production-dns-link" {
+  name                  = "production-dns-link"
+  private_dns_zone_name = azurerm_private_dns_zone.database.name
+  virtual_network_id    = data.azurerm_virtual_network.ms_production.id
+  resource_group_name   = azurerm_resource_group.fs_flov2.name
+}
+
+resource "azurerm_postgresql_flexible_server" "staging" {
+  name                          = "staging-flo"
+  resource_group_name           = azurerm_resource_group.fs_flov2.name
+  location                      = var.azure_location
+  version                       = "13"
+  delegated_subnet_id           = azurerm_subnet.database.id
+  private_dns_zone_id           = azurerm_private_dns_zone.database.id
+  geo_redundant_backup_enabled  = true
+  administrator_login           = "qxlva"
+  administrator_password        = data.azurerm_key_vault_secret.pg_staging_pass.value
+  zone                          = "1"
+  public_network_access_enabled = false
+
+  storage_mb = 32768
+
+  sku_name   = "GP_Standard_D2ds_v4"
+  depends_on = [azurerm_private_dns_zone_virtual_network_link.database]
+}
+
+resource "azurerm_postgresql_flexible_server_configuration" "staging" {
+  name      = "azure.extensions"
+  server_id = azurerm_postgresql_flexible_server.staging.id
+  value     = "POSTGIS,UUID-OSSP"
+}
+
+resource "azurerm_postgresql_flexible_server" "live" {
+  name                          = "live-flo"
+  resource_group_name           = azurerm_resource_group.fs_flov2.name
+  location                      = var.azure_location
+  version                       = "13"
+  delegated_subnet_id           = azurerm_subnet.database.id
+  private_dns_zone_id           = azurerm_private_dns_zone.database.id
+  geo_redundant_backup_enabled  = true
+  administrator_login           = "qxlva"
+  administrator_password        = data.azurerm_key_vault_secret.pg_live_pass.value
+  zone                          = "1"
+  public_network_access_enabled = false
+
+  storage_mb = 32768
+
+  sku_name   = "GP_Standard_D2ds_v4"
+  depends_on = [azurerm_private_dns_zone_virtual_network_link.database]
+}
+
+resource "azurerm_postgresql_flexible_server_configuration" "live" {
+  name      = "azure.extensions"
+  server_id = azurerm_postgresql_flexible_server.live.id
+  value     = "POSTGIS,UUID-OSSP"
+}
+
+resource "azurerm_postgresql_flexible_server" "migrate" {
+  name                          = "migrate-flo"
+  resource_group_name           = azurerm_resource_group.fs_flov2.name
+  location                      = var.azure_location
+  version                       = "13"
+  delegated_subnet_id           = azurerm_subnet.database.id
+  private_dns_zone_id           = azurerm_private_dns_zone.database.id
+  geo_redundant_backup_enabled  = true
+  administrator_login           = "qxlva"
+  administrator_password        = data.azurerm_key_vault_secret.pg_migrate_pass.value
+  zone                          = "1"
+  public_network_access_enabled = false
+
+  storage_mb = 32768
+
+  sku_name   = "GP_Standard_D2ds_v5"
+  depends_on = [azurerm_private_dns_zone_virtual_network_link.database]
+}
+
+resource "azurerm_postgresql_flexible_server_configuration" "migrate" {
+  name      = "azure.extensions"
+  server_id = azurerm_postgresql_flexible_server.migrate.id
+  value     = "POSTGIS,UUID-OSSP"
+}
---
+++ source/terraform/platform/storage.tf
@@ -0,0 +1,99 @@
+resource "azurerm_storage_account" "dev" {
+  name                             = "devflo"
+  resource_group_name              = azurerm_resource_group.fs_flov2.name
+  location                         = var.azure_location
+  account_tier                     = "Standard"
+  account_replication_type         = "GRS"
+  cross_tenant_replication_enabled = true
+
+  tags = {
+    environment = "development"
+  }
+}
+
+resource "azurerm_storage_container" "dev" {
+  name = "devflo"
+  # storage_account_name  = azurerm_storage_account.dev.name
+  storage_account_id    = azurerm_storage_account.dev.id
+  container_access_type = "blob"
+}
+
+resource "azurerm_storage_account" "test" {
+  name                             = "testingflo"
+  resource_group_name              = azurerm_resource_group.fs_flov2.name
+  location                         = var.azure_location
+  account_tier                     = "Standard"
+  account_replication_type         = "GRS"
+  cross_tenant_replication_enabled = true
+
+  tags = {
+    environment = "testing"
+  }
+}
+
+resource "azurerm_storage_container" "test" {
+  name = "testingflo"
+  #storage_account_name  = azurerm_storage_account.test.name
+  storage_account_id    = azurerm_storage_account.test.id
+  container_access_type = "blob"
+}
+
+resource "azurerm_storage_account" "stage" {
+  name                             = "stageflo"
+  resource_group_name              = azurerm_resource_group.fs_flov2.name
+  location                         = var.azure_location
+  account_tier                     = "Standard"
+  account_replication_type         = "GRS"
+  cross_tenant_replication_enabled = true
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_storage_container" "stage" {
+  name = "stageflo"
+  #storage_account_name  = azurerm_storage_account.stage.name
+  storage_account_id    = azurerm_storage_account.stage.id
+  container_access_type = "blob"
+}
+
+resource "azurerm_storage_account" "live" {
+  name                             = "liveflo"
+  resource_group_name              = azurerm_resource_group.fs_flov2.name
+  location                         = var.azure_location
+  account_tier                     = "Standard"
+  account_replication_type         = "GRS"
+  cross_tenant_replication_enabled = true
+
+  tags = {
+    environment = "live"
+  }
+}
+
+resource "azurerm_storage_container" "live" {
+  name = "liveflo"
+  #storage_account_name  = azurerm_storage_account.live.name
+  storage_account_id    = azurerm_storage_account.live.id
+  container_access_type = "blob"
+}
+
+resource "azurerm_storage_account" "migrate" {
+  name                             = "migrateflo"
+  resource_group_name              = azurerm_resource_group.fs_flov2.name
+  location                         = var.azure_location
+  account_tier                     = "Standard"
+  account_replication_type         = "GRS"
+  cross_tenant_replication_enabled = true
+
+  tags = {
+    environment = "migrate"
+  }
+}
+
+resource "azurerm_storage_container" "migrate" {
+  name = "migrateflo"
+  #storage_account_name  = azurerm_storage_account.migrate.name
+  storage_account_id    = azurerm_storage_account.migrate.id
+  container_access_type = "blob"
+}
